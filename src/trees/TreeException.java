7fa7c9866d82ff9b340207f69ed42c4132354427
Japji <japjikbatra@gmail.com>
2024-04-26 09:54:58 -0700

    updated TreeNode class

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -1,10 +1,10 @@
 package trees;
 
-public class TreeException extends FamilyTree
+public class TreeException extends Exception
 {
 	public TreeException(String s)
 	{
 		// Pass s into a superclass ctor.
-		super();
+		super(s);
 	}
 }
